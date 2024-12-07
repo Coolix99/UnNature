bcad4a09f017514bbfd4fb57516b32eb5120e2cc
Nablaaa <Eric_Schmidt_99@gmx.de>
2024-12-07 18:23:50 +0100

    modify error message

--- src_server/config.py
+++ src_server/config.py
@@ -7,9 +7,11 @@ class Config:
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = config("MAIL_USERNAME", default="anonymous")
-    MAIL_PASSWORD = config("MAIL_PASSWORD", default="anonymous")
-    MAIL_DEFAULT_SENDER = config("MAIL_USERNAME", default=None)
+    MAIL_USERNAME = config("MAIL_USERNAME", default=None)
+    MAIL_PASSWORD = config("MAIL_PASSWORD", default=None)
+    MAIL_DEFAULT_SENDER = MAIL_USERNAME
 
     if not MAIL_USERNAME or not MAIL_PASSWORD:
-        raise ValueError("MAIL_USERNAME and MAIL_PASSWORD must be set.")
+        raise ValueError(
+            "The senders GMAIL MAIL_USERNAME and MAIL_PASSWORD must be set."
+        )
